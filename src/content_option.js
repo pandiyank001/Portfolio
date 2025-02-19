77d058a98d2dde99c4adf03ad7058075cf346a0f
Pandiyan K <pandiyan.k@codincity.com>
2025-02-19 14:54:23 +0530

    Initial commit - portfolio code

---
+++ src/content_option.js
@@ -0,0 +1,161 @@
+const logotext = "Pandiyan K";
+const meta = {
+    title: "Pandiyan K",
+    description: "I’m Pandiyan K Full stack devloper,currently working in Codincity Digital Technologies, Coimbatore",
+};
+
+const introdata = {
+    title: "I’m Pandiyan K",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I love cloud computing",
+    },
+    description: " I work closely with cross-functional teams to design, develop, and maintain scalable web applications.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "I am a passionate Full-Stack Engineer and Cloud & DevOps Enthusiast with expertise in designing and developing scalable web applications. With a strong foundation in ReactJS, NodeJS, and NestJS, I specialize in building high-performance front-end and back-end solutions using JavaScript, TypeScript, and Java.",
+};
+const worktimeline = [{
+        jobtitle: "Full Stack engineer",
+        where: "Codincity Digital Technologies",
+        date: "2024",
+    },
+    {
+        jobtitle: "Software Engineer Intern",
+        where: "Aludra technology solutions",
+        date: "2023",
+    },
+   
+];
+
+const skills = [{
+        name: "ReactJs",
+        value: 95,
+    },
+    {
+        name: "Typescript",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 95,
+    },
+    {
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "NodeJs",
+        value: 90,
+    },
+    {
+        name: "Nestjs",
+        value: 85,
+    },
+    {
+        name: "Sql",
+        value: 85,
+    },
+    {
+        name: "Azure",
+        value: 80,
+    },
+    {
+        name: "Devops",
+        value: 80,
+    },
+    
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [
+
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The Business Website",
+        link: "https://github.com/pandiyankrishna/sample-business-website",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The Stock Market Analysis",
+        link: "https://github.com/pandiyankrishna/stockmarketanalysis",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The React Calculator",
+        link: "https://github.com/pandiyankrishna/react-calculator",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The Skin disease Prediction",
+        link: "https://github.com/pandiyankrishna/Skin-Disease-Prediction",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The Library Management Api",
+        link: "https://github.com/pandiyank001/Library_management_api_postman",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The Birthday mail.",
+        link: "https://github.com/pandiyank001/birthday_mail_python",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The Docker For Nodejs",
+        link: "https://github.com/pandiyank001/Devopsassessment",
+    },
+   
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/pandiyank001",
+    },
+   
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "pandiyankrishna2001@gmail.com",
+    YOUR_FONE: "9003658149",
+    description: " I work closely with cross-functional teams to design, develop, and maintain scalable web applications.",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/pandiyank001",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/pandiyank01/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
